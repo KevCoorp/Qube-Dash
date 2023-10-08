714ef44e0ec89977869156e4c1a2a36e56070a33
KevFR <ikevfr@gmail.com>
2023-10-08 10:00:01 +0200

    Import project

---
+++ Player.gd
@@ -0,0 +1,49 @@
+extends KinematicBody2D
+
+export(int) var JUMP_FORCE = -140
+export(int) var JUMP_RELEASE_FORCE = -70
+export(int) var MAX_SPEED = 50
+export(int) var ACCELERATION = 10 
+export(int) var FRICTION = 10
+export(int) var GRAVITY = 4
+export(int) var ADDITIONAL_FALL_GRAVITY = 4 
+
+var velocity = Vector2.ZERO
+
+# Mouvement du joueur
+func _physics_process(delta):
+	apply_gravity()
+	var input = Vector2.ZERO
+	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if input.x == 0:
+		apply_friction()
+	else: 
+		apply_acceleration(input.x)
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_FORCE
+		else:
+			
+			if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
+				velocity.y = JUMP_RELEASE_FORCE
+		
+			if velocity.y > 0:
+				velocity.y += ADDITIONAL_FALL_GRAVITY
+	
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+# Gravité 
+func apply_gravity():
+		velocity.y += GRAVITY
+		velocity.y = min(velocity.y, 300)
+	
+# Friction
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, FRICTION)
+
+func apply_acceleration(amount):
+	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
+	
