fa5bfc826662402624860083462e74416603f02e
KevFR <ikevfr@gmail.com>
2023-10-09 05:18:01 +0200

    Improve stuff

--- WalkingEnemy.gd
+++ WalkingEnemy.gd
@@ -3,12 +3,15 @@ extends KinematicBody2D
 var direction = Vector2.RIGHT
 var velocity = Vector2.ZERO
 
+onready var ledgeCheckRight: = $LedgeCheckRight
+onready var ledgeCheckLeft: = $LedgeCheckLeft
 
-# Mouvement
 func _physics_process(delta):
 	var found_wall = is_on_wall()
-	if found_wall:
-		direction += -1	
+	var found_ledge = not ledgeCheckRight.is_colliding() or not ledgeCheckLeft.is_colliding()
+	
+	if found_wall or found_ledge:
+		direction *= -1
 	
 	velocity = direction * 25
 	move_and_slide(velocity, Vector2.UP)
