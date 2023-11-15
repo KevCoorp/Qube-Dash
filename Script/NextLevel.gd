7c09b36611c7da84119755003674f04aa8472870
KevFR <ikevfr@gmail.com>
2023-11-15 22:24:21 +0100

    level transition

--- Script/NextLevel.gd
+++ Script/NextLevel.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+export (String, FILE, "*.tscn") var target_level_path = ""
+
 func _on_NextLevel_body_entered(body):
-	if body.is_in_group("Player"):
-		print("test")	
-	
+	if not body is Player: return
+	if target_level_path.empty(): return
+	get_tree().change_scene(target_level_path)
