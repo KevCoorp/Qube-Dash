f7e7589565e868eea7254be6ff6d0f421a7324dc
KevFR <ikevfr@gmail.com>
2023-11-16 20:49:13 +0100

    add animation and work on transition

--- Script/NextLevel.gd
+++ Script/NextLevel.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
+onready var animatedSprite = $AnimatedSprite
+
 export (String, FILE, "*.tscn") var target_level_path = ""
 
 func _on_NextLevel_body_entered(body):
 	if not body is Player: return
 	if target_level_path.empty(): return
 	get_tree().change_scene(target_level_path)
+	animatedSprite.play("Check")
+
+
