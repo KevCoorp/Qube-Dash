04b227616f021f53997d57629bd529bf935da0f5
KevFR <ikevfr@gmail.com>
2023-11-19 20:53:19 +0100

    level design

--- Script/NextLevel.gd
+++ Script/NextLevel.gd
@@ -2,12 +2,22 @@ extends Area2D
 
 onready var animatedSprite = $AnimatedSprite
 
+var time_in_seconds = 1
+
 export (String, FILE, "*.tscn") var target_level_path = ""
 
 func _on_NextLevel_body_entered(body):
 	if not body is Player: return
 	if target_level_path.empty(): return
-	get_tree().change_scene(target_level_path)
 	animatedSprite.play("Check")
+	yield(get_tree().create_timer(time_in_seconds), "timeout")
+	get_tree().change_scene(target_level_path)
+
+
+
+
+
+	
+
 
 
