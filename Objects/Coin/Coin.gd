b4e36e546fc109314c44f3860d62933f2c7d6b4a
KevFR <ikevfr@gmail.com>
2024-01-21 12:43:59 +0100

    level design

--- Objects/Coin/Coin.gd
+++ Objects/Coin/Coin.gd
@@ -6,6 +6,7 @@ func _on_Coin_body_entered(body):
 	$AnimationPlayer.play("pick_up")
 	emit_signal("coin_collected")
 	set_collision_mask_bit(0,false)
+	$SoundCoinCollect.play()
 	
 func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
