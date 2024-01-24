13a5d3b7ad9eab10fe354cee54e72a301dd3ae28
KevFR <ikevfr@gmail.com>
2024-01-24 15:46:35 +0100

    improve test

--- Level1.gd
+++ Level1.gd
@@ -4,7 +4,12 @@ const PlayerScene = preload("res://Player.tscn")
 
 var bg = Color("#3363CD")
 
-func _ready():
+onready var visibility_notifier := $VisibilityNotifier2D
+
+
+func _ready() -> void:
 	VisualServer.set_default_clear_color(bg)
-	
+	visibility_notifier.connect("screen_entered", self, "show")
+	visibility_notifier.connect("screen_exited", self, "hide")
+	visible = false
 
