c53036c68c0e92f0a3da1610fe99a00ee08e1436
KevFR <ikevfr@gmail.com>
2024-07-16 18:44:43 +0200

    working on a point system

---
+++ scripts/misc/gamemanager/GameManager.gd
@@ -0,0 +1,8 @@
+extends Control
+
+var score = 0
+
+func add_point():
+	score += 1
+	print(score)
+
--- scripts/sprite/item/coin/coin.gd
+++ scripts/sprite/item/coin/coin.gd
@@ -1,9 +1,4 @@
 extends Area2D
 
-func collected():
-	pass
-	
 func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		collected()
-		queue_free()
+	pass
