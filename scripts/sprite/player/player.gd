6999cbf8091270c3501d20c26ba982d1450972fa
KevFR <ikevfr@gmail.com>
2024-07-29 14:28:33 +0200

    add jump sfx effect

--- scripts/sprite/player/player.gd
+++ scripts/sprite/player/player.gd
@@ -4,6 +4,8 @@ class_name Player
 const SPEED = 150.0
 const JUMP_VELOCITY = -355
 
+@onready var audio = $Jump
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
@@ -13,6 +15,7 @@ func _physics_process(delta):
 	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		audio.play()
 
 	var direction = Input.get_axis("left","right")
 	if direction:
