08d2a18ef039c4f54bbceb707cddcff4f238cf34
KevFR <ikevfr@gmail.com>
2024-07-29 15:08:43 +0200

    some improve

--- scripts/misc/gamemanager/GameManager.gd
+++ scripts/misc/gamemanager/GameManager.gd
@@ -1,14 +1,16 @@
 extends Node
 
-signal add_point(int)
+signal gained_coins(int)
 
-var coins 
+var coins : int
 
 var score = 0
 
 var player : Player
 
-func gain_coins(coins_gained):
+func gain_coins(coins_gained:int):
 	coins += coins_gained
 	emit_signal("gained_coins", coins_gained)
+	
+
 
--- scripts/sprite/item/coin/coin.gd
+++ scripts/sprite/item/coin/coin.gd
@@ -1,4 +1,8 @@
-extends Area2D
+extends Node2D
 
-func _on_area_entered(area):
-	pass
+@onready var coins = $CoinsSFX
+
+func _on_area_2d_body_entered(body):
+	coins.play()
+	GameManager.gain_coins(1)
+	queue_free()
